453415a05c36d57de4b7d5a03408105817261255
Sergey Makarov <makar1031@yandex.ru>
2018-12-16 17:31:26 +0300

    исправление конфликтов

--- src/js/reducers/pet.js
+++ src/js/reducers/pet.js
@@ -14,35 +14,23 @@ export default function pet(state = initialState, action) {
       alertify.error('Я не голодна');
       return state;
     case 'PET_PLAYING_ERROR':
-      alertify.error('Я больше не хочу играть(');
+      alertify.error('Я больше не хочу играть');
       return state;
     case 'PET_HAD_PLAYED':
       alertify.success('Крууууть');
       return action.data;
-<<<<<<< HEAD
     // case 'PET_SLEEPING':
     //   alertify.success('i dont wanna sleep anymore')
     //   return action.data;
     case 'PET_SLEEPING_ERROR':
-      alertify.success('Я не хочу спать')
-=======
-    case 'PET_SLEEPING':
-      alertify.success('i dont wanna sleep anymore')
-      return action.data;
-    case 'PET_SLEEPING_ERROR':
-      alertify.success('i dont wanna sleep anymore');
->>>>>>> ef04c30465df51d1993ff341a47890d63446e878
+      alertify.success('Я не хочу спать');
       return state;
     case 'PET_TREATED':
       alertify.success('Спасибо');
       return action.data;
     case 'PET_TREATING_ERROR':
-<<<<<<< HEAD
-      alertify.error('Я полностью здорова')
-=======
-      alertify.error('im healthy af');
+      alertify.error('Я полностью здорова');
       return state;
->>>>>>> ef04c30465df51d1993ff341a47890d63446e878
     default:
       return state;
   }
