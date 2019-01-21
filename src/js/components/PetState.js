67505c70c6737b3342de6ee1fd18d72fdfece4df
Sergey Makarov <makar1031@yandex.ru>
2019-01-21 18:21:10 +0300

    исправлена работа и частота поллинга

--- src/js/components/PetState.js
+++ src/js/components/PetState.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { describeDisease, load } from '../actions'
+import React, {Component} from "react";
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {describeDisease, load} from '../actions'
 import Indicator from './Indicator';
 
 import unicornImage from "../../img/sparkle.png";
@@ -10,22 +10,18 @@ class PetState extends Component {
 
   componentDidMount() {
     this.props.load();
-    this.timer = setInterval(() => {
-      if (this.props.pet.name) {
-        this.props.load();
-      }
-    }, 5000);
+    this.setState({
+      pollTimer: setInterval(() => this.props.load(), 10000)
+    });
   }
 
   componentWillUnmount() {
-    this.timer = null;
+    clearInterval(this.state.pollTimer);
   }
-
 
   showHealthIndicator = () => this.props.pet.disease != null;
 
   healthIndicatorHandler = () => this.props.describeDisease(this.props.pet.disease);
-
 
   render() {
     return (
@@ -33,21 +29,18 @@ class PetState extends Component {
         <div className="col-12">
           <div className="col-3">
             <Indicator name='Здоровье' value={this.props.pet.health}
-              showIndicator={this.showHealthIndicator()}
-              indicatorHandler={this.healthIndicatorHandler} />
-            <Indicator name='Голод' value={this.props.pet.hunger} />
-            <Indicator name='Настроение' value={this.props.pet.mood} />
-            <Indicator name='Отдых' value={this.props.pet.rest} />
+                       showIndicator={this.showHealthIndicator()}
+                       indicatorHandler={this.healthIndicatorHandler}/>
+            <Indicator name='Голод' value={this.props.pet.hunger}/>
+            <Indicator name='Настроение' value={this.props.pet.mood}/>
+            <Indicator name='Отдых' value={this.props.pet.rest}/>
           </div>
           <div className="row justify-content-md-center animated zoomInDown">
-            <img style={{ height: 300 }} src={unicornImage} alt="" />
+            <img style={{height: 300}} src={unicornImage} alt=""/>
           </div>
-          <div
-            className="row justify-content-md-center p-4 font-weight-bold animated fadeIn">{this.props.pet.name} (Счет: {this.props.pet.score})
+          <div className="row justify-content-md-center p-4 font-weight-bold animated fadeIn">
+            {this.props.pet.name} (Счет: {this.props.pet.score})
           </div>
-          {/* <div
-            className="row justify-content-md-center p-4 font-weight-bold animated fadeIn">{this.props.pet.score}
-          </div> */}
         </div>
       </div>
     );
@@ -59,7 +52,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(
-  { load, describeDisease },
+  {load, describeDisease},
   dispatch
 );
 
