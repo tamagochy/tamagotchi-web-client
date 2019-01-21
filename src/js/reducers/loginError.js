f237249914bc2b00ff4186fa2e8ba81632aa07d3
Sergey Makarov <makar1031@yandex.ru>
2019-01-21 22:44:20 +0300

    обновлен внешний вид форм

--- src/js/reducers/loginError.js
+++ src/js/reducers/loginError.js
@@ -5,6 +5,9 @@ export default function loginError(state = initialState, action) {
     case 'AUTH_SUCCESS':
       return initialState;
     case 'AUTH_FAILED':
+      if (action.data[0].code === "business.BadCredentials") {
+        return "Имя или пароль неправильные";
+      }
       return "Не удалось войти, что-то пошло не так";
     default:
       return state;
